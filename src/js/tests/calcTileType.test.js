3e608e76615c54eb783cec9faa776708352076f4
Polina Titova <titova_polina@mail.ru>
2022-03-16 00:53:39 +0300

    Task 4 completed

---
+++ src/js/tests/calcTileType.test.js
@@ -0,0 +1,20 @@
+import { calcTileType } from '../utils';
+
+test.each([
+  [0, 'top-left'],
+  [7, 'top-right'],
+  [2, 'top'],
+  [16, 'left'],
+  [23, 'right'],
+  [56, 'bottom-left'],
+  [63, 'bottom-right'],
+  [59, 'bottom'],
+  [10, 'center'],
+])(
+  'tile type test',
+  (index, expected) => {
+    const boardSize = 8;
+    const result = calcTileType(index, boardSize);
+    expect(result).toBe(expected);
+  },
+);
