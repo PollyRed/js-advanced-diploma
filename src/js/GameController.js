e8073114b7366db487bd336244b1dacb33a63671
Polina Titova <titova_polina@mail.ru>
2022-03-20 03:15:25 +0300

    Tasks 9 and 10 (1/2) completed

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -6,6 +6,8 @@ import Swordsman from './Swordsman';
 import Undead from './Undead';
 import Vampire from './Vampire';
 import { generatePositions, generatePositionedTeam } from './generators';
+import GameState from './GameState';
+import GamePlay from './GamePlay';
 
 const playerAllowedTypes = [Bowman, Magician, Swordsman];
 const computerAllowedTypes = [Daemon, Undead, Vampire];
@@ -24,6 +26,8 @@ export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+    this.positions = [];
+    this.gameState = new GameState();
   }
 
   init() {
@@ -38,22 +42,97 @@ export default class GameController {
     const playerTeam = generatePositionedTeam(playerAllowedTypes, 1, 2, playerInitPositions);
     const computerTeam = generatePositionedTeam(computerAllowedTypes, 1, 2, computerInitPositions);
 
-    const positions = playerTeam.concat(computerTeam);
+    this.positions = playerTeam.concat(computerTeam);
 
-    this.gamePlay.redrawPositions(positions);
+    this.gamePlay.redrawPositions(this.positions);
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
+
+    this.addListeners();
+  }
+
+  getCharacterFromCell(index) {
+    for (const position of this.positions) {
+      if (position.position === index) {
+        return position;
+      }
+    }
+
+    return null;
+  }
+
+  static getCharacterInfo(character) {
+    return `🎖${character.level} ⚔${character.attack} 🛡${character.defence} ❤${character.health}`;
+  }
+
+  static isPlayableCharacter(character) {
+    return character.type === 'bowman' || character.type === 'swordsman' || character.type === 'magician';
+  }
+
+  isCharacterCanAttack(index) {
+    return Math.abs(this.gameState.selectedCharacter.position - index) === 1
+           || Math.abs(this.gameState.selectedCharacter.position - index) === 7
+           || Math.abs(this.gameState.selectedCharacter.position - index) === 8
+           || Math.abs(this.gameState.selectedCharacter.position - index) === 9;
+  }
+
+  addListeners() {
+    this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
+    this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this));
+    this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
   }
 
   onCellClick(index) {
-    // TODO: react to click
+    const character = this.getCharacterFromCell(index);
+    if (this.gameState.isPlayerTurn && character !== null) {
+      if (GameController.isPlayableCharacter(character.character)) {
+        if (this.gameState.selectedCell >= 0) {
+          this.gamePlay.deselectCell(this.gameState.selectedCell);
+          this.gameState.selectedCell = -1;
+        }
+        this.gamePlay.selectCell(index);
+        this.gameState.selectedCell = index;
+        this.gameState.selectedCharacter = character;
+      } else {
+        GamePlay.showError('Это неигровой персонаж!');
+      }
+    }
   }
 
   onCellEnter(index) {
-    // TODO: react to mouse enter
+    const character = this.getCharacterFromCell(index);
+    if (character !== null) {
+      if (this.gameState.selectedCell !== -1 && this.gameState.selectedCell !== index) {
+        if (GameController.isPlayableCharacter(character.character)) {
+          this.gamePlay.setCursor('pointer');
+        } else {
+          if (this.isCharacterCanAttack(index)) {
+            this.gamePlay.setCursor('crosshair');
+            this.gamePlay.selectCell(index, 'red');
+          } else {
+            this.gamePlay.setCursor('not-allowed');
+          }
+        }
+      }
+      const characterInfo = GameController.getCharacterInfo(character.character);
+      this.gamePlay.showCellTooltip(characterInfo, index);
+    } else if (character === null
+              && this.gameState.selectedCell !== -1
+              && this.gameState.selectedCell !== index) {
+      if (this.isCharacterCanAttack(index)) {
+        this.gamePlay.setCursor('pointer');
+        this.gamePlay.selectCell(index, 'green');
+      } else {
+        this.gamePlay.setCursor('not-allowed');
+      }
+    }
   }
 
   onCellLeave(index) {
-    // TODO: react to mouse leave
+    this.gamePlay.setCursor('auto');
+    if (index !== this.gameState.selectedCell) {
+      this.gamePlay.deselectCell(index);
+    }
+    this.gamePlay.hideCellTooltip(index);
   }
 }
--- src/js/GameState.js
+++ src/js/GameState.js
@@ -1,4 +1,9 @@
 export default class GameState {
+  constructor() {
+    this.isPlayerTurn = true;
+    this.selectedCell = -1;
+    this.selectedCharacter = null;
+  }
   static from(object) {
     // TODO: create object
     return null;
